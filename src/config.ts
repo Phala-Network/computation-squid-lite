3fbef3ccb0c6f006164d3f9e4af5c4ff9277d902
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2023-05-24 18:06:51 +0800

    khala update

--- src/config.ts
+++ src/config.ts
@@ -4,18 +4,12 @@ import {
   type DataSource,
 } from '@subsquid/substrate-processor'
 
-type Network = 'khala' | 'phala'
-
-const NETWORK: Network = 'khala'
-
 const config: {
-  network: Network
   dataSource: DataSource
   blockRange: Exclude<BlockRangeOption['range'], undefined>
 } = {
-  network: NETWORK,
   blockRange: {from: 3670131},
-  dataSource: {archive: lookupArchive(NETWORK)},
+  dataSource: {archive: lookupArchive('khala')},
 }
 
 export default config
